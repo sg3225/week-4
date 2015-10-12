05746008d7ffe9ccbf3da6634009795181842c2e
sg3225 <shengyeguo@dyn-160-39-10-169.dyn.columbia.edu>
2015-10-12 00:47:09 -0400

    0930-T4-streaming-sg3225

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+from flask import Response
+from Queue import Queue
+
 
 import json
 import time
@@ -10,6 +13,16 @@ import random
 import pyorient
 
 app = Flask(__name__)
+q = Queue()
+
+def event_stream():
+    while True:
+        result = q.get()
+        yield 'data: %s\n\n' % str(result)
+@app.route('/eventSource/')
+def sse_source():
+    return Response( event_stream(), mimetype='text/event-stream' )
+
 
 @app.route("/")
 def index():
@@ -17,6 +30,7 @@ def index():
 
 @app.route("/getData/")
 def getData():
+        q.put("starting data query...")
 
 	lat1 = str(request.args.get('lat1'))
 	lng1 = str(request.args.get('lng1'))
@@ -26,8 +40,8 @@ def getData():
 	print "received coordinates: [" + lat1 + ", " + lat2 + "], [" + lng1 + ", " + lng2 + "]"
 	
 	client = pyorient.OrientDB("localhost", 2424)
-	session_id = client.connect("root", "password")
-	db_name = "property_test"
+	session_id = client.connect("root", "A43B7554D92A15A4B8C52F38B1236F0ECA680D07F2B8446E6512A6BDD6CD7D2B")
+	db_name = "soufun"
 	db_username = "admin"
 	db_password = "admin"
 
@@ -60,7 +74,8 @@ def getData():
 		feature["geometry"]["coordinates"] = [record.latitude, record.longitude]
 
 		output["features"].append(feature)
-
+        q.put('idle')
+        
 	return json.dumps(output)
 
 if __name__ == "__main__":
